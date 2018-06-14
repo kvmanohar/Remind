55ea663ab9912862ef0e2c66564514ce6a76d918
Manohar Kurapati <kvmanohar@gmail.com>
2018-06-14 20:03:53 +0100

    Initial Commit

---
+++ Remind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind
+//
+//  Created by Manohar Kurapati on 14/06/2018.
+//  Copyright © 2018 Manosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
